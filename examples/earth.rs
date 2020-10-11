0c9e08938de6a7028ec1f7beb5ce8d46cdd11e15
adamsky <adamsky@enum.space>
2020-10-11 20:47:37 +0200

    earth example: implement mouse dragging

--- examples/earth.rs
+++ examples/earth.rs
@@ -1,3 +1,14 @@
+//! This example displays an earth globe based on the example ascii earth
+//! texture.
+//!
+//! # Mouse controls
+//!
+//! Click and drag to rotate the globe. Use the mouse wheel to zoom in and out.
+//!
+//! # Keyboard controls
+//!
+//! Use arrow keys to rotate, *PgUp* and *PgDown* to zoom.
+
 #![allow(warnings)]
 
 extern crate crossterm;
@@ -14,6 +25,8 @@ use crossterm::{
     ExecutableCommand, QueueableCommand,
 };
 
+use crossterm::event::MouseEvent;
+use crossterm::terminal::enable_raw_mode;
 use globe::{Camera, Canvas, Globe, GlobeConfig};
 
 fn main() {
@@ -21,6 +34,7 @@ fn main() {
 
     let mut stdout = stdout();
     stdout.execute(cursor::Hide);
+    stdout.execute(crossterm::event::EnableMouseCapture);
 
     let mut earth_texture_path = std::env::current_dir().unwrap();
     earth_texture_path.push("examples/ascii/earth.txt");
@@ -28,15 +42,20 @@ fn main() {
     let mut globe = GlobeConfig::new()
         .load_texture_from(earth_texture_path.to_str().unwrap())
         .build();
-    let mut canvas = Canvas::new(250, 250, None);
+    // let mut canvas = Canvas::new(450, 450, None);
+    let (w, h) = crossterm::terminal::size().unwrap();
+    let mut canvas = Canvas::new(h * 8, h * 8, None);
 
     let mut angle_offset = 0.;
     let mut cam_zoom = 2.;
     let mut cam_xy = 0.;
     let mut cam_z = 0.;
     globe.camera = Camera::new(cam_zoom, cam_xy, cam_z);
+
+    let mut last_drag_pos = None;
+
     loop {
-        if poll(Duration::from_millis(100)).unwrap() {
+        if poll(Duration::from_millis(10)).unwrap() {
             match read().unwrap() {
                 Event::Key(event) => match event.code {
                     KeyCode::Char(c) => return,
@@ -70,8 +89,32 @@ fn main() {
                     }
                     _ => (),
                 },
-                //Event::Mouse(event) => println!("{:?}", event),
-                //Event::Resize(width, height) => println!("New size {}x{}", width, height),
+                Event::Mouse(event) => match event {
+                    MouseEvent::Drag(_, x, y, _) => {
+                        if let Some(last) = last_drag_pos {
+                            let (x_last, y_last) = last;
+                            let x_diff = x as f64 - x_last as f64;
+                            let y_diff = y as f64 - y_last as f64;
+
+                            if y_diff > 0. && cam_z < 1.5 {
+                                cam_z += 0.1;
+                            } else if y_diff < 0. && cam_z > -1.5 {
+                                cam_z -= 0.1;
+                            }
+                            globe.angle += x_diff * globe::PI / 30.;
+                            globe.angle += y_diff * globe::PI / 30.;
+                        }
+                        last_drag_pos = Some((x, y))
+                    }
+                    MouseEvent::ScrollUp(..) => cam_zoom -= 0.1,
+                    MouseEvent::ScrollDown(..) => cam_zoom += 0.1,
+                    _ => last_drag_pos = None,
+                },
+                Event::Resize(width, height) => {
+                    let ratio = width as f64 / height as f64;
+                    canvas = Canvas::new((height * 6) as u16, height * 6, None);
+                    // 200 / 60 = 3.3
+                }
                 _ => (),
             }
         }
@@ -94,26 +137,12 @@ fn main() {
         stdout.execute(crossterm::terminal::Clear(
             crossterm::terminal::ClearType::FromCursorDown,
         ));
-        //println!(
-        //"camera.x: {}, camera.y: {}, camera.z: {}",
-        //globe.camera.x, globe.camera.y, globe.camera.z
-        //);
-        //println!(
-        //"cam_xy: {}, cam_z: {}, cam_zoom: {}, angle: {}",
-        //cam_xy, cam_z, cam_zoom, globe.angle
-        //);
-        gotoxy(0, 0);
+
+        stdout.execute(crossterm::cursor::MoveTo(0, 0));
 
         //update camera position
-        std::thread::sleep(std::time::Duration::from_millis(10));
+        // std::thread::sleep(std::time::Duration::from_millis(10));
         //globe.angle += 1. * globe::PI / 10.;
         //angle_offset += 0. * PI / 10.;
     }
-}
-
-fn gotoxy(x: u16, y: u16) {
-    //unimplemented!()
-    stdout().execute(crossterm::cursor::MoveTo(x, y));
-    //COORD coord = {x, y};
-    //SetConsoleCursorPosition ( GetStdHandle ( STD_OUTPUT_HANDLE ), coord );
 }
