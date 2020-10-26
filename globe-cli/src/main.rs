f2476019086ee31dd7a07b43d2fbc4ae5f696765
adamsky <adamsky@enum.space>
2020-10-27 00:10:14 +0100

    use clap

--- globe-cli/src/main.rs
+++ globe-cli/src/main.rs
@@ -1,13 +1,11 @@
-//! This example displays an earth globe based on the example ascii earth
-//! texture.
+//! Render an ASCII globe in your terminal.
 //!
-//! # Mouse controls
+//! # Interactive mode controls
 //!
-//! Click and drag to rotate the globe. Use the mouse wheel to zoom in and out.
+//! **Mouse**: click and drag to rotate the globe. Use the mouse wheel to zoom in
+//! and out.
 //!
-//! # Keyboard controls
-//!
-//! Use arrow keys to rotate, *PgUp* and *PgDown* to zoom.
+//! **Keyboard**: use arrow keys to rotate, *PgUp* and *PgDown* to zoom.
 
 #![allow(warnings)]
 
@@ -22,12 +20,121 @@ use crossterm::{
     style::Print,
     ExecutableCommand, QueueableCommand,
 };
-
 use crossterm::event::MouseEvent;
 use crossterm::terminal::enable_raw_mode;
 use globe::{Camera, Canvas, Globe, GlobeConfig, PI};
+use clap::{App, AppSettings, Arg};
+
+pub const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+pub const AUTHORS: &'static str = env!("CARGO_PKG_AUTHORS");
 
 fn main() {
+    let mut app = App::new("globe-cli")
+        .version(VERSION)
+        .author(AUTHORS)
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .about("Render an ASCII globe in your terminal.")
+        .arg(Arg::new("interactive").short('i'))
+        .arg(Arg::new("screensaver").short('s'));
+    let matches = app.get_matches();
+    if matches.is_present("interactive") {
+        start_interactive();
+    } else if matches.is_present("screensaver") {
+        start_screensaver();
+    }
+}
+
+fn start_screensaver() {
+    crossterm::terminal::enable_raw_mode().unwrap();
+
+    let mut stdout = stdout();
+    stdout.execute(cursor::Hide);
+    stdout.execute(cursor::DisableBlinking);
+    stdout.execute(crossterm::event::EnableMouseCapture);
+
+    let mut earth_texture_path = std::env::current_dir().unwrap();
+    earth_texture_path.push("globe-cli/ascii/earth.txt");
+    println!("{:?}", earth_texture_path);
+
+    let mut globe = GlobeConfig::new()
+        .load_texture_from(earth_texture_path.to_str().unwrap())
+        .build();
+    // let mut canvas = Canvas::new(450, 450, None);
+    let mut term_size = crossterm::terminal::size().unwrap();
+    let mut canvas = if term_size.0 > term_size.1 {
+        Canvas::new(term_size.1 * 8, term_size.1 * 8, None)
+    } else {
+        Canvas::new(term_size.0 * 4, term_size.0 * 4, None)
+    };
+
+    let mut angle_offset = 0.;
+    let mut cam_zoom = 2.;
+    let mut cam_xy = 0.;
+    let mut cam_z = 0.;
+    globe.camera = Camera::new(cam_zoom, cam_xy, cam_z);
+
+    loop {
+        if poll(Duration::from_millis(100)).unwrap() {
+            match read().unwrap() {
+                Event::Key(event) => match event.code {
+                    KeyCode::Char(c) => return,
+                    _ => (),
+                },
+                Event::Resize(width, height) => {
+                    term_size = (width, height);
+                    canvas = if width > height {
+                        Canvas::new(height * 8, height * 8, None)
+                    } else {
+                        Canvas::new(width * 4, width * 4, None)
+                    };
+                }
+                _ => (),
+            }
+        }
+
+        globe.angle += -1. * globe::PI / 50.;
+
+        globe.camera = Camera::new(cam_zoom, cam_xy, cam_z);
+
+        canvas.clear();
+
+        // render globe on the canvas
+        globe.render_on(&mut canvas);
+
+        // print canvas to terminal
+        let (sizex, sizey) = canvas.get_size();
+        for i in 0..sizey / 8 {
+            stdout.queue(crossterm::terminal::Clear(
+                crossterm::terminal::ClearType::CurrentLine,
+            ));
+            for j in 0..sizex / 4 {
+                stdout.queue(Print(canvas.matrix[i][j]));
+            }
+            // stdout.execute(cursor::MoveToNextLine(1));
+            stdout.queue(cursor::MoveDown(1));
+            stdout.queue(cursor::MoveLeft((sizex / 4) as u16));
+            stdout.flush().unwrap();
+        }
+
+        if term_size.0 / 2 > term_size.1 {
+            // center the cursor on the x axis
+            stdout.execute(crossterm::cursor::MoveTo(
+                (sizex / 8) as u16 - ((sizex / 8) / 4) as u16,
+                // (term_size.0 / 2) - (term_size.0 / 4) as u16,
+                // term_size.0 / 2,
+                0,
+            ));
+        }
+
+        //update camera position
+        // std::thread::sleep(std::time::Duration::from_millis(10));
+        // globe.angle += 1. * globe::PI / 10.;
+        // angle_offset += 0. * PI / 10.;
+
+    }
+}
+
+fn start_interactive() {
     crossterm::terminal::enable_raw_mode().unwrap();
 
     let mut stdout = stdout();
